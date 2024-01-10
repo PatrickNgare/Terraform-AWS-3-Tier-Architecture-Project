19d944fe1fa76e91883284b570ba778de685ad10
PatrickNgare <patrickngare@gmail.com>
2024-01-10 22:19:15 +0300

    added availability zone

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.vpc-solavise.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Solavise-public-subnet-1"
@@ -10,7 +11,10 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   vpc_id     = aws_vpc.vpc-solavise.id
   cidr_block = "10.0.2.0/24"
+ availability_zone = "us-east-1b"
 
+
+  
   tags = {
     Name = "Solavise-public-subnet-2"
   }
@@ -19,6 +23,8 @@ resource "aws_subnet" "public-subnet-2" {
 resource "aws_subnet" "private-subnet-1" {
   vpc_id     = aws_vpc.vpc-solavise.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
 
   tags = {
     Name = "Solavise-private-subnet-1"
@@ -28,6 +34,7 @@ resource "aws_subnet" "private-subnet-1" {
 resource "aws_subnet" "private-subnet-2" {
   vpc_id     = aws_vpc.vpc-solavise.id
   cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "Solavise-private-subnet-2"
