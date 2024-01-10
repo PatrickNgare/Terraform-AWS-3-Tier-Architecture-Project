ca917e65a59f3159b00a9a0d93e5c5779ce6753a
PatrickNgare <patrickngare@gmail.com>
2024-01-10 20:59:50 +0300

    added two app servers

--- ec2.tf
+++ ec2.tf
@@ -9,3 +9,35 @@ resource "aws_instance" "Webserver-1" {
     Name = "Webserver-1-solavise"
   }
 }
+
+resource "aws_instance" "Webserver-2" {
+
+  ami = "ami-0c7217cdde317cfec" 
+
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Webserver-2-solavise"
+  }
+}
+
+resource "aws_instance" "appserver-1" {
+
+  ami = "ami-0c7217cdde317cfec" 
+
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "appserver-1-solavise"
+  }
+}
+resource "aws_instance" "appserver-2" {
+
+  ami = "ami-0c7217cdde317cfec" 
+
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "appserver-2-solavise"
+  }
+}
