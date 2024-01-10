0196818e06e3acaf93638cb97476e89dce3c0c6f
PatrickNgare <patrickngare@gmail.com>
2024-01-10 21:46:07 +0300

    added 4 subnets

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,35 @@
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id     = aws_vpc.vpc-solavise.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Main"
+    Name = "Solavise-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id     = aws_vpc.vpc-solavise.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Solavise-public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id     = aws_vpc.vpc-solavise.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Solavise-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id     = aws_vpc.vpc-solavise.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "Solavise-private-subnet-2"
   }
 }
